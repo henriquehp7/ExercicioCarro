8d3e3b1d6c98bcaa64ea23c7d5e276e990731939
Henrique de paula <rickksdepaula@hotmail.com>
2018-04-10 11:51:46 -0300

    first commit

---
+++ app/src/main/java/br/com/fiap/exerciciocarro/CarroTres.java
@@ -0,0 +1,22 @@
+package br.com.fiap.exerciciocarro;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by logonrm on 10/04/2018.
+ */
+
+public class CarroTres extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.carro_tres,container,false);
+
+    }
+}
---
+++ app/src/main/java/br/com/fiap/exerciciocarro/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.fiap.exerciciocarro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirCamaro(View view) {
+    }
+
+    public void abrirFerrari(View view) {
+    }
+
+    public void abrirPorsche(View view) {
+    }
+}
